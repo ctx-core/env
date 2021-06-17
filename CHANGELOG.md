69685091ef0c255823eda9995defcc82e3cd17ae
Brian Takita <brian.takita@gmail.com>
2021-06-17 03:29:18 -0400

    ## 12.0.5
    ### Patch Changes
    
    - fix: is*staging\$\_b: eql* type issue

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # @ctx-core/env
 
+## 12.0.5
+
+### Patch Changes
+
+- fix: is*staging\$\_b: eql* type issue
+
 ## 12.0.4
 
 ### Patch Changes
--- src/is_staging$_b.ts
+++ src/is_staging$_b.ts
@@ -1,5 +1,5 @@
 import { B, be_ } from '@ctx-core/object'
-import { _eql } from '@ctx-core/function'
+import { eql_ } from '@ctx-core/function'
 import { derived$, Readable$ } from '@ctx-core/store'
 import { NODE_ENV$_b } from './NODE_ENV$_b'
 import type { env_Ctx } from './env_Ctx'
@@ -7,7 +7,7 @@ const key = 'is_staging$'
 export const is_staging$_b:B<env_Ctx, typeof key> = be_(key, ctx=>
 	derived$(
 		NODE_ENV$_b(ctx),
-		_eql('staging')
+		eql_(['staging'])
 	) as is_staging$_T
 )
 export type is_staging_T = boolean
