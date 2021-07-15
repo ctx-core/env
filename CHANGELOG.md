64e5dcb190a7462e4cedc5a08929c11612f9e263
Brian Takita <brian.takita@gmail.com>
2021-07-15 07:41:51 -0400

    ## 13.0.3
    ### Patch Changes
    
    - import: + .js extention

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # @ctx-core/env
 
+## 13.0.3
+
+### Patch Changes
+
+- import: + .js extention
+
 ## 13.0.2
 
 ### Patch Changes
--- src/env_Ctx.ts
+++ src/env_Ctx.ts
@@ -1,3 +1,3 @@
-import type { env_ctx_I } from './env_ctx_I.generated'
+import type { env_ctx_I } from './env_ctx_I.generated.js'
 export interface env_Ctx extends env_ctx_I {
 }
