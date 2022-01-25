92adb59b6435cf28b145d2a6549c6c41a8c0945f
Brian Takita <brian.takita@gmail.com>
2022-01-24 20:57:37 -0600

    remove () around single argument arrow functions

--- src/is_production$_b.js
+++ src/is_production$_b.js
@@ -3,7 +3,7 @@ import { computed$ } from '@ctx-core/nanostores'
 import { NODE_ENV$_b } from './NODE_ENV$_b.js'
 const key = 'is_production$'
 /** @type {import('is_production$_b').is_production$_b} */
-export const is_production$_b = be_(key, (ctx)=>
+export const is_production$_b = be_(key, ctx=>
 	computed$(NODE_ENV$_b(ctx),
 		(NODE_ENV)=>
 			NODE_ENV === 'prod' || NODE_ENV === 'production.js'
--- src/is_staging$_b.js
+++ src/is_staging$_b.js
@@ -4,7 +4,7 @@ import { computed$ } from '@ctx-core/nanostores'
 import { NODE_ENV$_b } from './NODE_ENV$_b.js'
 const key = 'is_staging$'
 /** @type {import('is_staging$_b').is_staging$_b} */
-export const is_staging$_b = be_(key, (ctx)=>
+export const is_staging$_b = be_(key, ctx=>
 	computed$(NODE_ENV$_b(ctx),
 		eql_([
 			'staging'
